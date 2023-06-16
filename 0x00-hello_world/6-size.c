72faa0312d3e82ed88acb3c338d54e81a56e5a46
stdjes <stephendjes@gmail.com>
2023-06-15 18:36:59 -0700

    Task 6 - print type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - marks the beginnignof execution of program
+ *
+ * Return: 0 when successful and exits program
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
