013371654e3bcf5045a87403150158ce9a4049d8
stdjes <stephendjes@gmail.com>
2023-06-16 22:56:23 -0700

    Task 7 - Mirror Alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * this program prints lower case alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
