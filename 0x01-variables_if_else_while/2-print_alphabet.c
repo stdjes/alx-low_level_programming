c9868158a6441ddfc2e9c1eb5b08fdc4eab658e4
stdjes <stephendjes@gmail.com>
2023-06-16 21:33:15 -0700

    Task 2 the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - begins program run
+ * this program prints the alphabest in lowercase,
+ * followed by a new line
+ * Return: 0 when program sucessful
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+		putchar('\n');
+	}
+	return (0);
+}
