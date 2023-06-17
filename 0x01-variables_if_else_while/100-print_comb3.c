1f0048d39f0bcbb8b0f9ac75b0ca278f6aad5599
stdjes <stephendjes@gmail.com>
2023-06-17 16:50:39 -0700

    Task 10 - Print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - beginning of program
+ * prints all possible combinations of two digit numbers
+ * without repetition, the the smallest number combination
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int d;
+
+	for (i = 0; i <= 9; i++) /* loop initialize condition for i*/
+	{
+		for (d = 0; d <= 9; d++) /* nested loop initialize condition for d*/
+		{
+			if (i != d && i < d) /* skip equal combinations, skip repeated combination*/
+			{
+				putchar (i + '0');
+				putchar (d + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		}
+	putchar('\n');
+	return (0);
+}
