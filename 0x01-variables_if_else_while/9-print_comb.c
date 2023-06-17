601e4061dd307744af691664cacc2586e11a7b3e
stdjes <stephendjes@gmail.com>
2023-06-17 15:22:11 -0700

    Task 9 - Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,12 @@ int main(void)
 	for (d = 0; d <= 9; d++) /* loops the print of numbers 0 -9 */
 	{
 		putchar(d + '0'); /* prints  d, after adding the ASCII value of 0 to d*/
+		if (d < 9)
+		{
 		putchar(','); /* inserts a comma after each digit*/
 		putchar(' '); /* inserts a space after the comma*/
+		}
 	}
-
 	putchar('\n');
 	return (0);
 
