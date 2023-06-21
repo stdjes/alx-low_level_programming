9a6bba60c9f38aa8de299343682ea8e6310f52bb
stdjes <stephendjes@gmail.com>
2023-06-21 14:44:10 -0700

    Task 2 final fix

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
+void print_alphabet_x10(void);
+
 /**
  * main - start of program.
  * this program prints the lower case alphabets 10x
  * Return: Always 0.
  */
+
 int main(void)
 {
     print_alphabet_x10();
