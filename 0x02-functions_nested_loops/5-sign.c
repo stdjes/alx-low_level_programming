a0f200ce6183d7f8ff78516cf13d6b1246889a05
stdjes <stephendjes@gmail.com>
2023-06-21 16:20:09 -0700

    Task 5 - Sign check

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - a function to print the sign of a given number
+ * @n: variable for the integer to be checked
+ *
+ * Return: 1(+ve), -1(-ve), 0(zero)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0 )
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
