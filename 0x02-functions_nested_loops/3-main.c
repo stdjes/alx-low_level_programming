9248dd92862972265b6b12ea43160373cac8ea55
stdjes <stephendjes@gmail.com>
2023-06-21 15:16:01 -0700

    Task 3 - is lower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int _islower(int c);
+/**
+ * main - function testing program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
