f4a4cb833f1177a8b59ef848997e49fa0b2344e1
stdjes <stephendjes@gmail.com>
2023-06-22 13:14:22 -0700

    1 - Digit detective

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - this function checks if input is a  digit or not
+ * @c: variable holder for this function
+ *
+ * Return: 1 for interger, 0 for non-integer.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
