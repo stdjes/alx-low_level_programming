33c3bcbd4f5827db8bd6413870cd9e25b7b3ff20
stdjes <stephendjes@gmail.com>
2023-06-22 14:17:23 -0700

    2 - Product of two integer  variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - computes the product of two integers
+ * @a: first integer variable
+ * @b: second integer variable
+ *
+ * Return: the value ot the product of the two integers
+ */
+int mul(int a, int b)
+{
+	int product = 0;
+
+	product = a * b;
+
+	return (product);
+}
