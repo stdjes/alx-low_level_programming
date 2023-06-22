0195884f05204f208c2ae02a45d9905c16261b14
stdjes <stephendjes@gmail.com>
2023-06-21 19:39:45 -0700

    Task 10 - Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - adds two numbers and returs the sum
+ *@a: first integer variable
+ *@b: second interger variable
+ *
+ * Return: sum of the intergers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
