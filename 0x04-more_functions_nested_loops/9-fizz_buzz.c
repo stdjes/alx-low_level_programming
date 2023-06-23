8ff691a63e27f353bfb69de5a693cf36aece147a
stdjes <stephendjes@gmail.com>
2023-06-22 20:02:28 -0700

    9 - Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,12 +16,12 @@ int main(void)
 	{
 		if (n == 100)
 			printf("Buzz");
+		else if (n % 15 == 0)
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+			printf("Fizz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
-		else if (n % 5 == 0)
-			printf("Buzz ");
-		else if (n % 15 == 0 && n != 100)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", n);
 	}
