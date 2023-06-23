bc71e732ed11b921bb63728b74b4b01ed37c6258
stdjes <stephendjes@gmail.com>
2023-06-22 21:23:41 -0700

    100 - Largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * this program computes and prints the largest prime factor of a number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	long int hf = 2;
+
+	while (n > hf)
+	{
+		if (n % hf == 0)
+		{
+			n /= hf; /* Reduce the value of n by dividing it by hf*/
+		}
+		else
+		{
+			hf++; /*Increment hf to check the next possible factor*/
+		}
+	}
+
+	printf("The largest prime factor of %ld is: %ld\n", n, hf);
+
+	return (0);
+}
