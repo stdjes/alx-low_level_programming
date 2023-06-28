cafc61df7300745a58260a3986fdafe4e36ba485
stdjes <stephendjes@gmail.com>
2023-06-28 07:36:10 -0700

    7 - pusts half fix***

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,9 +18,19 @@ void puts_half(char *str)
 	while (str[l] != '\0')
 		l++;
 
-	for (n = l / 2; n < l; n++)
+	if (l % 2 == 0)
 	{
-		_putchar(str[n]);
+		for (n = l / 2; n < l; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else if (l % 2 == 1)
+	{
+		for (n = (l - 1) / 2; n < l; n++)
+		{
+			_putchar(str[n]);
+		}
 	}
 	_putchar('\n');
 }
