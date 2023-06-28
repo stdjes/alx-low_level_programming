8a34e1e05050a56efb1b5c910d0dcd097a5a33b3
stdjes <stephendjes@gmail.com>
2023-06-27 23:00:00 -0700

    0 - 98 battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that resets a pointer value to 98
+ *
+ * @n: the pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05. C - Pointers, arrays and strings
+
+An introductory project to the concepts of pointers, arrays and strings.
