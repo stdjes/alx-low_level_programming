39ea1aec146fbe4a3db8cda210ba6275d56e3d8c
stdjes <stephendjes@gmail.com>
2023-06-29 00:29:44 -0700

    8 - Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 	int q;			/*position counter*/
 
 	if (n <= 0)
-		fprintf(stderr, "\n");
+		fprintf(stderr, "Invalid! Array elements must be >= 1");
 
 	for (q = 0; q < n - 1; q++)
 	{
