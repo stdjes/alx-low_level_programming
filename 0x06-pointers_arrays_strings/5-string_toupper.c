9726610d9dd142c0cc5869ce88bb82e34cac732b
stdjes <stephendjes@gmail.com>
2023-07-03 03:09:29 -0700

    5 To Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - coverts the lowwercase of a string to upper case
+ *
+ * @str: the sting variable to be converted
+ *
+ * Return: returns the upper case converted string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	size_t lenstr = strlen(str);
+	if (lenstr == 0)
+		return(NULL);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 'a' + 'A';
+		i++;
+	}
+	return (str);
+}
