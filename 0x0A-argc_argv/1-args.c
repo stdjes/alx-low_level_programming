ebed1e5b3c36feb6bc6e2ae783ce61f6fcc17b34
stdjes <stephendjes@gmail.com>
2023-07-07 08:22:54 -0700

    1 - print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start point of program
+ * this program prints the number count of arguments passed to it.
+ *
+ * @_argc: holds the arguments counter
+ * @_argv: argument value place holder
+ *
+ * Return: 0 (Success)
+ */
+int main(int _argc, char **_argv)
+{
+	printf("%d", _argc);
+	return (0);
+}
