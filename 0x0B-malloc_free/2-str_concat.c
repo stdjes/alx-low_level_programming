a462fb1fcd08f0475af0defea88053aa5ca20296
stdjes <stephendjes@gmail.com>
2023-07-10 18:00:09 -0700

    2 - String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = '\0';
 	if (s2 == NULL)
-		s2 = "";
+		s2 = '\0';
 
 	newStr = malloc((totalLen + 1) * sizeof(char));
 
