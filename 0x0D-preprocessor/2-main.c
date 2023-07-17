7a2301a0d82242d65f1073c49ebbb3199c9b57e1
stdjes <stephendjes@gmail.com>
2023-07-17 04:28:40 -0700

    2 - print File name Macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * this program prints the name of the source file, followed by a new line
+ *
+ * Return: 0, on sucessful run of the program
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
