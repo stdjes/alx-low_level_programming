9ed57c648c82220d59edb8332346c49de8cfd9f9
stdjes <stephendjes@gmail.com>
2023-07-17 20:35:50 -0700

    3 - new type definition for struct dog (dog_t)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,11 +20,11 @@ struct dog
 /**
  * my_dog - a new type definition for (struct dog)
  */
-typedef struct dog my_dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-my_dog *new_dog(char *name, float age, char *owner);
-void free_dog(my_dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
